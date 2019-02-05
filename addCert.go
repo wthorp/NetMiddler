8b7003f0771d18074bb7de428166d8849c46a212
Bill Thorp <billt@storj.io>
2019-02-04 22:05:50 -0500

    info on adding certs

--- addCert.go
+++ addCert.go
@@ -1,5 +1,8 @@
 package main
 
+
+/// todo: use https://github.com/FiloSottile/mkcert
+
 import (
 	"crypto/x509"
 	"fmt"
@@ -28,3 +31,18 @@ func main() {
 
 	rootCAs.AppendCertsFromPEM(caCert)
 }
+
+func loadSystemRoots() (*CertPool, error) {
+	// TODO: restore this functionality on Windows. We tried to do
+	// it in Go 1.8 but had to revert it. See Issue 18609.
+	// Returning (nil, nil) was the old behavior, prior to CL 30578.
+	return nil, nil
+
+	const CRYPT_E_NOT_FOUND = 0x80092004
+
+	store, err := syscall.CertOpenSystemStore(0, syscall.StringToUTF16Ptr("ROOT"))
+	if err != nil {
+		return nil, err
+	}
+	defer syscall.CertCloseStore(store, 0)
+] //CertCloseStore(hRootCertStore,0);
